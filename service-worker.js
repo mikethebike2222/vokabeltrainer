f5ba9e6170fd67a93719a1ffc5f93b2e303788c9
mikethebike2222 <michaelfalk2@web.de>
2025-08-12 22:54:24 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'vokabeltrainer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './service-worker.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install: Assets cachen
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+// Activate: alte Caches aufräumen
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: erst Cache, dann Netz (Fallback)
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => 
+      cached || fetch(req).then(res => {
+        // nur GET und gleiche Origin cachen
+        if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        }
+        return res;
+      }).catch(() => cached)
+    )
+  );
+});
